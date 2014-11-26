877ddd4958ba3dd435532d7f8647e527511ab026
Ji Hun Kim <jk2227@cornell.edu>
2014-11-26 16:57:15 -0500

    A little progress... see comments in game.ml

--- release/game/game.ml
+++ release/game/game.ml
@@ -12,15 +12,34 @@ let game_datafication g = match g with
 	
 let game_from_data game_data = Game (game_data)
 
-let handle_step g ra ba =
-	failwith 
-    "Remember my super cool Rattata? My Rattata is different from regular 
-    Rattata. It’s like my Rattata is in the top percentage of all Rattata."
+let handle_step g ra ba = 
+    (*
+        check if it's in pool of avail steammons
+        if it is check that you have enough creds
+        if you do then add it to the respective player's
+        list of steammon's and sent pickrequest to appropriate player
+        if not choose the lowest cost or something... i don't 
+        know the details well for that case
+    *)
+  let draft color ((rsl,ri,rcred),(bsl,bi,bcred)) name = 
+    match color with 
+    | Red -> failwith "todo" (* (None, ...) *)
+    | Blue -> failwith "todo" (* (None, ...) *)
+  in
 
-                        (* Youngster Joey, about his Raticate *)
+  match g, ra, ba with 
+  | Game gsd, Action(SendTeamName (rName)), Action (SendTeamName (bName)) ->
+    (None, gsd, Some(Request(PickRequest(Red,gsd,
+        hash_to_list (Initialization.move_table), 
+        hash_to_list(Initialization.mon_table)))), None)
+  | Game gsd, Action(PickSteammon name), DoNothing -> draft Red gsd name
+  | Game gsd, DoNothing, Action(PickSteammon name) -> draft Blue gsd name  
+  | _ -> failwith "swag"
 
+ 
 let init_game () =
     init_pool ("moves.csv") ("steammon.csv");
-    (Game(([],[],0),([],[],0)),TeamNameRequest,TeamNameRequest, 
+    (Game(([],[],cSTEAMMON_CREDITS),([],[],cSTEAMMON_CREDITS)),
+        TeamNameRequest,TeamNameRequest, 
         hash_to_list (Initialization.move_table), 
         hash_to_list(Initialization.mon_table))
